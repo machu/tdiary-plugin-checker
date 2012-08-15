8767235cfc8981660eb3cd88ddcef5cbb7d58a76
MATSUOKA Kohei <kohei@machu.jp>
2012-08-15 09:17:12 +0900

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdiaryPluginChecker::Application.config.secret_token = '74732bd379fd5386e88ca0f7f07991f6a36c77f89d1dbfddae2057306c0a0441c9def66a73185c7fc788a94a32fe0ec600e5414a0a6bd92c074bded8f1717433'
