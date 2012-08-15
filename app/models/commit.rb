934491d1de25f053306b35fe3e454b088833de05
MATSUOKA Kohei <kohei@machu.jp>
2012-08-15 10:50:17 +0900

    fetch commits from GitHub

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,3 +1,21 @@
+require 'octokit'
+
 class Commit < ActiveRecord::Base
   attr_accessible :date, :message, :sha, :tree, :url
+  validates_uniqueness_of :sha
+
+  def Commit.fetch
+    Octokit.commits('tdiary/tdiary-contrib').each do |c|
+      unless Commit.where(sha: c.sha).size > 0
+        commit = Commit.create(
+          date: c.commit.author.date,
+          message: c.commit.message,
+          tree: c.commit.tree.sha,
+          url: c.url,
+          sha: c.sha
+        )
+      end
+    end
+    nil
+  end
 end
