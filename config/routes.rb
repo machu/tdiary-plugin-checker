896c50d0dda153857e4a4973725a6184f395068c
MATSUOKA Kohei <kohei@machu.jp>
2012-08-16 20:22:27 +0900

    set TDiaryPluginFiles#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ TdiaryPluginChecker::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 't_diary_plugin_files#index'
 
   # See how all your routes lay out with "rake routes"
 
