fe45e7edf115d736cce00e8ae5049d8b00b29603
MATSUOKA Kohei <kohei@machu.jp>
2012-08-16 20:22:01 +0900

    sort plugins by commit date

--- app/controllers/t_diary_plugin_files_controller.rb
+++ app/controllers/t_diary_plugin_files_controller.rb
@@ -2,8 +2,9 @@ class TDiaryPluginFilesController < ApplicationController
   # GET /t_diary_plugin_files
   # GET /t_diary_plugin_files.json
   def index
-    @t_diary_plugin_files = TDiaryPluginFile.all
-
+    @t_diary_plugin_files = TDiaryPluginFile.includes(:commit).sort!{|a,b|
+      b.commit.date <=> a.commit.date
+    }
     respond_to do |format|
       format.html # index.html.erb
       # format.json { render json: @t_diary_plugin_files }
