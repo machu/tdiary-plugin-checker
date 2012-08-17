a5e9470eed25e5e54fae9654a3829d3f476868b5
MATSUOKA Kohei <kohei@machu.jp>
2012-08-17 23:41:17 +0900

    add json link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 TdiaryPluginChecker::Application.routes.draw do
   resources :t_diary_plugin_files
-
-  resources :commits
+  root :to => 't_diary_plugin_files#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,7 +52,6 @@ TdiaryPluginChecker::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 't_diary_plugin_files#index'
 
   # See how all your routes lay out with "rake routes"
 
